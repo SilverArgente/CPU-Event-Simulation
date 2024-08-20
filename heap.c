a0898b08abcb4d38ce9b6bb2a11d7d08b4d60428
SilverArgente <dalalamogh@protonmail.com>
2024-08-20 13:01:59 -0400

    added project

---
+++ heap.c
@@ -0,0 +1,155 @@
+/*
+ * Min Heap Priority Queue
+ * By Amogh Dalal
+ * Created for CS435 HW1
+ * 7/10/24
+*/
+
+#include <stdlib.h>
+
+struct Task {
+    /* This user-defined structure is complete */
+    int id;
+    int priority;
+    int arrivalTime;
+    int processingTime;
+};
+
+struct Node {
+    struct Task task;
+    int priority;
+};
+
+struct PQ {
+    /* Add attributes to this user-defined structure */
+    struct Node *heap;
+    int size;
+    int max_size;
+};
+
+struct PQ PQ_new(void) {
+    /* Construct an empty heap */
+    
+    struct PQ pq;
+
+    pq.heap = malloc(sizeof(struct Node) * 32);
+    pq.size = 0;
+    pq.max_size = 32;
+
+    return pq;
+
+}
+
+void PQ_clear(struct PQ* pq) {
+    /* Remove all elements from the heap, effectively clearing it */
+    
+    free(pq->heap);
+    pq->max_size = 32;
+    pq->size = 0;
+    
+}
+
+int PQ_empty(struct PQ* pq) {
+    /* Return 1 if the heap is empty; otherwise, return 0 */
+    
+    return pq->size == 0;
+
+}
+
+void PQ_enqueue(struct PQ* pq, struct Task element, int priority) {
+    /* Insert the specified element into the heap with the given priority */
+    
+    struct Node node;
+    int i;
+
+    node.task = element;
+    node.priority = priority;
+
+    pq->size += 1;
+    i = pq->size;
+
+    /* If heap memory is full, reallocate more memory */
+    if (i == pq->max_size)
+    {
+	pq->max_size += 32;
+        pq->heap = realloc(pq->heap, sizeof(struct Node) * pq->max_size);
+    }
+    
+    pq->heap[i] = node;
+    
+    /* Heapify new element up */
+    while (i > 1 && pq->heap[i/2].priority > pq->heap[i].priority)
+    {
+	struct Node temp = pq->heap[i/2];
+	pq->heap[i/2] = pq->heap[i];
+	pq->heap[i] = temp;
+	i = i/2;
+    } 
+
+}
+
+void PQ_min_heapify(struct PQ *pq, int i)
+{
+    
+    int mindex;
+
+    /* Compare parent with left child to find minimum */ 
+    if (2*i <= pq->size && pq->heap[2*i].priority < pq->heap[i].priority)
+    {
+	mindex = 2 * i;
+    }
+    else
+    {
+        mindex = i;
+    }
+
+    /* Compare newly found minimum with right child */
+    if (2*i + 1 <= pq->size && pq->heap[2*i+1].priority < pq->heap[mindex].priority)
+    {
+        mindex = 2 * i + 1;
+    }
+    
+    /* Recursively percolate down */ 
+    if (mindex != i)
+    {
+        struct Node temp = pq->heap[i];
+        pq->heap[i] = pq->heap[mindex];
+	pq->heap[mindex] = temp;
+        PQ_min_heapify(pq, mindex);
+    }
+
+}
+
+struct Task PQ_dequeue(struct PQ* pq) {
+    /* Retrieve and remove the highest-priority element from the heap */
+    
+    struct Node node;
+    struct Task task;
+    int size;
+
+    node = pq->heap[1];
+    task = node.task;
+    size = pq->size;
+
+    pq->heap[1] = pq->heap[size];
+    pq->size -= 1;
+
+    PQ_min_heapify(pq, 1);
+
+    return task;
+
+}
+
+struct Task PQ_front(struct PQ* pq) {
+    /* Retrieve the highest-priority element without removing it */
+    
+    return pq->heap[1].task;
+
+}
+
+int PQ_priority(struct PQ* pq) {
+    /* Retrieve the priority of the highest-priority element on the heap */
+    
+    return pq->heap[1].priority;
+
+}
---
+++ simulation.c
@@ -0,0 +1,141 @@
+/* 
+ * Discrete Time CPU Event Simulation
+ * By Amogh Dalal
+ * Created for CS435 HW1
+ * 7/10/24
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "heap.c"
+
+int main(int argc, char **argv) 
+{
+   
+    FILE *file;
+    struct PQ pq;
+    struct PQ id_pq;
+    char line[256];
+    
+    struct Task *jobs;
+    int jobs_size = 0;
+    int jobs_max_size = 16;
+    int index = 0;
+
+    struct Task job;
+    int job_finish_time;
+    int job_wait_time;
+
+    int cpu_time = 0;
+    int is_cpu_free = 1;
+    
+    pq = PQ_new();
+    id_pq = PQ_new(); 
+    file = fopen(argv[argc-1], "r");
+    jobs = malloc(sizeof(struct Task) * jobs_max_size);
+
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        struct Task task;
+        char *field;
+
+        field = strtok(line, ",");
+        task.id = atoi(field); 
+    
+        field = strtok(NULL, ",");
+        task.priority = atoi(field);
+
+        field = strtok(NULL, ",");
+        task.arrivalTime = atoi(field);
+
+        field = strtok(NULL, ",");
+        task.processingTime = atoi(field);
+        
+	jobs[jobs_size] = task;
+	jobs_size++;
+	
+	if (jobs_size == jobs_max_size)
+	{
+            jobs_max_size += 16;
+	    jobs = realloc(jobs, sizeof(struct Task) * jobs_max_size);
+	}
+
+    }
+    
+    while (1)
+    {
+
+	// printf("time: %d\n", cpu_time);
+
+	/* Push all jobs at arrival time into priority queue */ 
+	while (jobs[index].arrivalTime == cpu_time && index < jobs_size)
+	{
+	    PQ_enqueue(&pq, jobs[index], jobs[index].priority);
+	    // printf("Pushed job of id: %d\n", jobs[index].id);
+	    index++;
+        }
+
+	/* Exit simulation */
+	if (index == jobs_size && PQ_empty(&pq) && is_cpu_free)
+	{
+	    // printf("Exiting simulation...\n");
+            break;
+	}
+	
+	/* If CPU is free, start processing job */
+	if (is_cpu_free && !PQ_empty(&pq))
+	{
+	    int priority;
+
+	    // printf("Heap size: %d\n", pq.size);
+	    // printf("Heap: %d %d %d %d\n", pq.heap[1].task.id, pq.heap[2].task.id, pq.heap[3].task.id, pq.heap[4].task.id);
+            job = PQ_dequeue(&pq);
+	    // printf("CPU is free, processing job with id: %d\n", job.id);
+	    priority = job.priority;
+	    PQ_enqueue(&id_pq, job, job.id);
+
+	    while (priority == PQ_front(&pq).priority && !PQ_empty(&pq))
+	    {
+		struct Task temp = PQ_dequeue(&pq);
+		// printf("same priority job detected, popping job with id: %d\n", temp.id);
+	    	// printf("Heap size: %d\n", pq.size);
+                PQ_enqueue(&id_pq, temp, temp.id);
+	    }
+		
+	    /* Order jobs with same priority by lowest id */
+	    job = PQ_dequeue(&id_pq);
+
+	    while (!PQ_empty(&id_pq))
+	    {
+	        struct Task temp = PQ_dequeue(&id_pq);
+		PQ_enqueue(&pq, temp, temp.priority);
+	    }
+            
+	    job_finish_time = cpu_time + job.processingTime;  
+	    job_wait_time = cpu_time - job.arrivalTime; 
+	    
+	    is_cpu_free = 0;
+	}
+        
+        /* Finish processing job */	
+	if (cpu_time == job_finish_time) // exit condition to prvent infinite loop)
+	{
+	    // printf("finished processing job with id: %d\n", job.id);
+	    printf("%d,%d,%d,%d,%d\n", job.id, job.arrivalTime, job.processingTime, cpu_time, job_wait_time);
+            is_cpu_free = 1;
+	    continue;
+	}	
+    
+        cpu_time++;
+    
+    } 
+
+    fclose(file);
+    free(jobs);
+    PQ_clear(&pq);
+
+    return 0;
+
+}
+
